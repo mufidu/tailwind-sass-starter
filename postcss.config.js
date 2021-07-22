e5e01c538b5af363d19b00ed9473e7308c4f5597
mufidu <mufid.to@gmail.com>
2021-07-22 21:33:46 +0700

    Fix postcss config
    Seems like cssnano is used during development.
    Edited postcss.config.js so cssnano plugin is not exported
    if NODE_ENV variable is not set to "production".
    
    Hope it works.

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,22 @@
-module.exports = {
-    plugins: [
-        require("postcss-import"),
-        require("tailwindcss"),
-        require("autoprefixer"),
-        require("postcss-nested"),
-        require("cssnano")({
-            preset: "default",
-        }),
-    ],
-};
+if (process.env.NODE_ENV === "production") {
+    module.exports = {
+        plugins: [
+            require("postcss-import"),
+            require("tailwindcss"),
+            require("autoprefixer"),
+            require("postcss-nested"),
+            require("cssnano")({
+                preset: "default",
+            }),
+        ],
+    };
+} else {
+    module.exports = {
+        plugins: [
+            require("postcss-import"),
+            require("tailwindcss"),
+            require("autoprefixer"),
+            require("postcss-nested"),
+        ],
+    };
+}
