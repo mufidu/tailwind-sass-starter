e6207b70c525d558a1c13788c9f15da426dd7ee8
Muhammad Mufid Utomo <70360519+mufidu@users.noreply.github.com>
2021-07-22 21:34:49 +0700

    Revert "Fix postcss config"

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,11 @@
-if (process.env.NODE_ENV === "production") {
-    module.exports = {
-        plugins: [
-            require("postcss-import"),
-            require("tailwindcss"),
-            require("autoprefixer"),
-            require("postcss-nested"),
-            require("cssnano")({
-                preset: "default",
-            }),
-        ],
-    };
-} else {
-    module.exports = {
-        plugins: [
-            require("postcss-import"),
-            require("tailwindcss"),
-            require("autoprefixer"),
-            require("postcss-nested"),
-        ],
-    };
-}
+module.exports = {
+    plugins: [
+        require("postcss-import"),
+        require("tailwindcss"),
+        require("autoprefixer"),
+        require("postcss-nested"),
+        require("cssnano")({
+            preset: "default",
+        }),
+    ],
+};
